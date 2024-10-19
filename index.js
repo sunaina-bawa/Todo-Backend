51dd75a545250434196bdc78be23411bd44086af
sunaina <bawasunaina6@gmail.com>
2024-10-19 15:17:58 +0530

    done

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/todos", todoRoutes);
-
+app.get("/",(req,res)=>{
+    res.send("todo app");
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
